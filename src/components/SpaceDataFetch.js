d52324054abfeb032445040fd0efcc44e475b3fb
Sjouke91 <s.bosma1991@gmail.com>
2020-10-14 09:55:48 +0200

    Added api data request

--- src/components/SpaceDataFetch.js
+++ src/components/SpaceDataFetch.js
@@ -1,5 +1,21 @@
-import React, { useState, UseEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function SpaceDataFetch() {
-  return <div></div>;
+  const [LatestData, setLatestData] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      console.log("Im going to fetch data");
+      const response = await axios.get(
+        "https://api.spacexdata.com/v2/launches"
+      );
+      console.log(response.data);
+      setLatestData(response.data);
+    }
+    getData();
+  }, []);
+  return <div>{/* <h1>{LatestData.name}</h1> */}</div>;
 }
+
+console.log("I'm run");
